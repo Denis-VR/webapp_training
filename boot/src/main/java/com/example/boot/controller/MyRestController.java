b0ffdb7887da1a3eaa7802d06ce9f3f60274b505
Denis Ryabchikov <ryabchickov18@bk.ru>
2021-05-12 09:47:07 +0300

    Add JPA

--- boot/src/main/java/com/example/boot/controller/MyRestController.java
+++ boot/src/main/java/com/example/boot/controller/MyRestController.java
@@ -3,8 +3,8 @@ package com.example.boot.controller;
 import com.example.boot.entity.Employee;
 import com.example.boot.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
 
 import java.util.List;
 
@@ -21,30 +21,30 @@ public class MyRestController {
 		return allEmployees;
 	}
 
-//	@GetMapping("/employees/{id}")
-//	public Employee getEmployee(@PathVariable int id) {
-//		Employee employee = employeeService.getEmployee(id);
-//
-//		return employee;
-//	}
+	@GetMapping("/employees/{id}")
+	public Employee getEmployee(@PathVariable int id) {
+		Employee employee = employeeService.getEmployee(id);
 
-//	@PostMapping("/employees")
-//	public Employee addNewEmployee(@RequestBody Employee employee) {
-//		employeeService.saveEmployee(employee);
-//		return employee;
-//	}
-//
-//	@PutMapping("/employees")
-//	public Employee updateEmployee(@RequestBody Employee employee) {
-//
-//		employeeService.saveEmployee(employee);
-//		return employee;
-//	}
-//
-//	@DeleteMapping("/employees/{id}")
-//	public String deleteEmployee(@PathVariable int id) {
-//
-//		employeeService.deleteEmployee(id);
-//		return "Employee with ID = " + id + " was deleted";
-//	}
+		return employee;
+	}
+
+	@PostMapping("/employees")
+	public Employee addNewEmployee(@RequestBody Employee employee) {
+		employeeService.saveEmployee(employee);
+		return employee;
+	}
+
+	@PutMapping("/employees")
+	public Employee updateEmployee(@RequestBody Employee employee) {
+
+		employeeService.saveEmployee(employee);
+		return employee;
+	}
+
+	@DeleteMapping("/employees/{id}")
+	public String deleteEmployee(@PathVariable int id) {
+
+		employeeService.deleteEmployee(id);
+		return "Employee with ID = " + id + " was deleted";
+	}
 }
--- boot/src/main/java/com/example/boot/dao/EmployeeDAOImpl.java
+++ boot/src/main/java/com/example/boot/dao/EmployeeDAOImpl.java
@@ -1,12 +1,12 @@
 package com.example.boot.dao;
 
-import org.hibernate.Session;
-import org.hibernate.query.Query;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import com.example.boot.entity.Employee;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import java.util.List;
 
 @Repository
@@ -17,32 +17,26 @@ public class EmployeeDAOImpl implements EmployeeDAO {
 
 	@Override
 	public List<Employee> getAllEmployees() {
-		Session session = entityManager.unwrap(Session.class);
-		Query<Employee> query = session.createQuery("from Employee", Employee.class);
-		List<Employee> allEmployees = query.getResultList();
+		Query query = entityManager.createQuery("from Employee");
 
-		return allEmployees;
+		return (List<Employee>) query.getResultList();
 	}
 
-//	@Override
-//	public void saveEmployee(Employee employee) {
-//		Session session = entityManager.getCurrentSession();
-//
-//		session.saveOrUpdate(employee);
-//	}
-//
-//	@Override
-//	public Employee getEmployee(int id) {
-//		Session session = entityManager.getCurrentSession();
-//		return session.get(Employee.class, id);
-//	}
-//
-//	@Override
-//	public void deleteEmployee(int id) {
-//		Session session = entityManager.getCurrentSession();
-//		Query<Employee> query = session.createQuery("delete from Employee " +
-//				"where id =:employeeId");
-//		query.setParameter("employeeId", id);
-//		query.executeUpdate();
-//	}
+	@Override
+	public void saveEmployee(Employee employee) {
+		Employee newEmployee = entityManager.merge(employee);
+		employee.setId(newEmployee.getId());
+	}
+
+	@Override
+	public Employee getEmployee(int id) {
+		return entityManager.find(Employee.class, id);
+	}
+
+	@Override
+	public void deleteEmployee(int id) {
+		Query query = entityManager.createQuery("delete from Employee where id =:employeeId");
+		query.setParameter("employeeId", id);
+		query.executeUpdate();
+	}
 }
